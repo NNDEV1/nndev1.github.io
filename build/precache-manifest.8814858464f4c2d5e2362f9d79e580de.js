0a5fc3ebf47c43798ffc9b490117d07a0f34f96e
nalinbot <nalinnagar1@gmail.com>
2022-04-24 23:54:12 -0400

    Add files via upload

---
+++ build/precache-manifest.8814858464f4c2d5e2362f9d79e580de.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6124befb8595e9c375c8048f4d7f6808",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4274acc9cc77a7c723c3",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "6508a84ad86aefe33036",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "81915979f6572af6cf4a",
+    "url": "/static/css/main.1a6005f5.chunk.css"
+  },
+  {
+    "revision": "16ff9d470cb2327dea63",
+    "url": "/static/js/2.78068198.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.78068198.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4274acc9cc77a7c723c3",
+    "url": "/static/js/3.a397ab76.chunk.js"
+  },
+  {
+    "revision": "6508a84ad86aefe33036",
+    "url": "/static/js/4.20d71466.chunk.js"
+  },
+  {
+    "revision": "81915979f6572af6cf4a",
+    "url": "/static/js/main.51be0035.chunk.js"
+  },
+  {
+    "revision": "9f06ef8efb3bbde7debd",
+    "url": "/static/js/runtime-main.c2e91519.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "4e57203982c8a9eac2247c883241ee3b",
+    "url": "/static/media/codlyLogo.4e572039.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "2334c2518b6a57baf92e81fff77f9f48",
+    "url": "/static/media/enloeLogo.2334c251.jpeg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "c5ceddd916d183ab05bae3e79565af87",
+    "url": "/static/media/qandmedicaidLogo.c5ceddd9.png"
+  },
+  {
+    "revision": "f6492351d81ae8c9570d96b3e1064cb6",
+    "url": "/static/media/satelliteLogo.f6492351.jpeg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
